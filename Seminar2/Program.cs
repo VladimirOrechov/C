8757481e280132bfc1df3479005947f8299a7fc1
Vladimir <jidkov91@gmail.com>
2023-04-13 18:42:20 +0300

    first commit

---
+++ Seminar2/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 5;
+
+int b = 6;
+
+int c = 7;
+
+int g = 8;
+
+int result1 = Calculete(a,b);
+int result2 = Calculete(c,g);
+
+Console.WriteLine(result1);
+Console.WriteLine(result2);
+
+int Calculete (int a, int b)
+{
+    return a * (a * b);
+}
+
+void Show (int a, int b)
+{
+    Console.WriteLine($"{a}, {b}");
+}
+
