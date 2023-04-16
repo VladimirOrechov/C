06e4e22176098003f80f946cb4aec6ba4af4e696
Vladimir <jidkov91@gmail.com>
2023-04-16 16:37:44 +0300

    first commit

--- HomeWork1.4/Program.cs
+++ HomeWork1.4/Program.cs
@@ -4,3 +4,17 @@
 8 -> 2, 4, 6, 8
 */
 
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+
+int i = 1;
+
+while (i <= N)
+if (i % 2 == 0)
+{
+Console.WriteLine();
+    {
+        ++i;
+    }
+}
+    
