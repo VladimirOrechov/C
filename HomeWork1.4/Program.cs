67ccb48750fda74b8913a00899b7e282722ecb46
Vladimir <jidkov91@gmail.com>
2023-04-16 19:03:32 +0300

    1

--- HomeWork1.4/Program.cs
+++ HomeWork1.4/Program.cs
@@ -7,14 +7,15 @@
 Console.WriteLine("Введите число N");
 int N = int.Parse(Console.ReadLine());
 
-int i = 1;
+int i = (1);
 
-while (i <= N)
-if (i % 2 == 0)
+while(i <= N);
 {
-Console.WriteLine();
+if (% i/2 == 0);
     {
-        ++i;
-    }
+    Console.WriteLine(i);
+    } 
+    ++i;
 }
     
+ 
