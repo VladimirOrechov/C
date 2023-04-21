033afa2085f1713524f6908c3974468f2005a267
Vladimir <jidkov91@gmail.com>
2023-04-21 22:05:17 +0300

    finished

--- example11/Program.cs
+++ example11/Program.cs
@@ -20,11 +20,35 @@ void PrintArray(int[] col)
     }
 }
 
+int indexof(int[] collection, int find)
+{
+    int counte = collection.Length;
+    int index = 0;
+    int position = -1;
+    while(index < counte)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
 
 int[] array = new int[10];
 
 FillArray(array);
+array[4] = 4;
+array[6] = 4;
 PrintArray(array);
+Console.WriteLine();
+
+int pos = indexof(array, 444);
+Console.WriteLine(pos);
+
+
 
 
 
