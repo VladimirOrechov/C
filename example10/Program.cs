668c61fd0fb48b89ef42ec7618b94957d8302b31
Vladimir <jidkov91@gmail.com>
2023-04-21 21:35:51 +0300

    finished

---
+++ example10/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 11,211,31,41,15,61,17,18,19 };
+
+int n = array.Length;
+int find = 15;
+
+int index = 0;
+
+
+while(index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+ index++;
+}
