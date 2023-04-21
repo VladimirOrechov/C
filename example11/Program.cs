79156f0a7dd2e8cfaabaa3beb8e8120a3aca5962
Vladimir <jidkov91@gmail.com>
2023-04-21 21:51:24 +0300

    finished

---
+++ example11/Program.cs
@@ -0,0 +1,31 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+    
+}
+void PrintArray(int[] col)
+{
+    int counte = col.Length;
+    int position = 0;
+    while(position < counte)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+
+
+
+
