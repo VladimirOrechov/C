a72eaa6adb52aea85003eadb317b7ddf26e9df3f
Vladimir <jidkov91@gmail.com>
2023-04-21 18:24:58 +0300

    add arg

--- example8/Program.cs
+++ example8/Program.cs
@@ -1,4 +1,4 @@
-﻿int Max(int arg1, int arg2, int arg3);
+﻿int Max(int arg1, int arg2, int arg3)
 {
     int result = arg1;
     if(arg2 > result) result = arg2;
@@ -11,21 +11,13 @@ int b1 = 21;
 int c1 = 39;
 int a2 = 38;
 int b2 = 276;
-int c2 = 30;
+int c2 = 300;
 int a3 = 152;
 int b3 = 258;
 int c3 = 39;
 
-int max = a1;
-if (b1 > max) max = b1;
-if (c1 > max) max = c1;
-
-if (a2 > max) max = a2;
-if (b2 > max) max = b2;
-if (c2 > max) max = c2;
-
-if (a3 > max) max = a3;
-if (b3 > max) max = b3;
-if (c3 > max) max = c3;
-
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
 Console.WriteLine(max);
